f85dc28f4fbdbfe37f86b9ec4680680cb71320f9
Luciano-tb <lucianotburg@gmail.com>
2024-08-26 21:52:58 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp01/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingrese el radio del cilindro:");
+                double radio = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese la altura del cilindro: ");
+                double altura = double.Parse(Console.ReadLine());
+
+                if (radio > 0 || altura > 0)
+                {
+                    double volumen = Math.PI * Math.Pow(radio, 2) * altura;
+                    double superficie = 2 * Math.PI * Math.Pow(radio, 2) + 2 * Math.PI * radio * altura;
+
+                    Console.WriteLine("Datos del cilindro:");
+                    Console.WriteLine($"Radio: {radio}");
+                    Console.WriteLine($"Altura: {altura}");
+                    Console.WriteLine($"Volumen: {volumen}");
+                    Console.WriteLine($"Superficie: {superficie}");
+                }
+                else
+                {
+                    Console.WriteLine("Los valores de radio y altura deben ser MAYORES que cero!!!");
+                }
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine("Algo se ingreso mal!!!");
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp02/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Radio: ");
+                    double radio = ;
+
+                    Console.Write("Altura: ");
+                    double altura = ;
+
+                    if (radio > 0 || altura > 0)
+                    {
+                        double volumen = Math.PI * radio * radio * altura;
+                        double superficie = 2 * Math.PI * radio * (radio + altura);
+
+                        Console.WriteLine($"Volumen: {volumen}");
+                        Console.WriteLine($"Superficie: {superficie}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Los valores de radio y altura deben ser MAYORES que cero!!!");
+                    }
+                    Console.Write("¿Otro cilindro? : ");
+
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Algo se ingreso mal!!!");
+            }
+        }
+        }
+    }
+}
